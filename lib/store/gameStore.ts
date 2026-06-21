ee31630145c336e13d4bbe663c6decdb2dc1f34d
adhambatlouni <adhamalbatlouni@gmail.com>
2026-06-21 12:30:04 +0300

    add persist zustand

--- lib/store/gameStore.ts
+++ lib/store/gameStore.ts
@@ -134,7 +134,6 @@ export const useGameStore = create<GameStore>()(
         score: state.score,
         history: state.history,
         gameOverReason: state.gameOverReason,
-        status: state.status,
       }),
     }
   )
